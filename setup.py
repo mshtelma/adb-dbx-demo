620dc86d9b86de0255abb7feea94938377214fd9
Michael Shtelma <mshtelma@gmail.com>
2022-12-12 17:35:10 +0100

    Initial Commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+"""
+This file configures the Python package with entrypoints used for future runs on Databricks.
+
+Please follow the `entry_points` documentation for more details on how to configure the entrypoint:
+* https://setuptools.pypa.io/en/latest/userguide/entry_point.html
+"""
+
+from setuptools import find_packages, setup
+from adb_dbx_demo import __version__
+
+PACKAGE_REQUIREMENTS = ["pyyaml"]
+
+# packages for local development and unit testing
+# please note that these packages are already available in DBR, there is no need to install them on DBR.
+LOCAL_REQUIREMENTS = [
+    "pyspark",
+    "delta-spark",
+    "scikit-learn",
+    "pandas",
+    "mlflow",
+]
+
+TEST_REQUIREMENTS = [
+    # development & testing tools
+    "pytest",
+    "coverage[toml]",
+    "pytest-cov",
+    "dbx>=0.7,<0.8"
+]
+
+setup(
+    name="adb_dbx_demo",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    setup_requires=["setuptools","wheel"],
+    install_requires=PACKAGE_REQUIREMENTS,
+    extras_require={"local": LOCAL_REQUIREMENTS, "test": TEST_REQUIREMENTS},
+    entry_points = {
+        "console_scripts": [
+            "etl = adb_dbx_demo.tasks.sample_etl_task:entrypoint",
+            "ml = adb_dbx_demo.tasks.sample_ml_task:entrypoint",
+    ]},
+    version=__version__,
+    description="",
+    author="",
+)
